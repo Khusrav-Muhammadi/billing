6f485324f46cc61b3bafe13bdde88a21bf4743ef
Khusrav-Muhammadi <khusravmuhammadi.73@gmail.com>
2025-01-18 13:29:44 +0500

    fg

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth.basic')->group(function () {
+    Route::get('clients-balance', [\App\Http\Controllers\ClientController::class, 'getBalance']);
 });
