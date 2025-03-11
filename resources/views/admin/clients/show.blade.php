4063eacdb7a43e47d8b10bf149868577c9055ee7
root <tfaiziev04@gmail.com>
2025-03-11 15:54:19 +0500

    changess

--- resources/views/admin/clients/show.blade.php
+++ resources/views/admin/clients/show.blade.php
@@ -541,7 +541,7 @@
                         <div style="display: flex; justify-content: space-between;">
                             <h4>{{ $history->status }}</h4>
                             <span>
-                                <strong>{{ $history->user->name }}</strong> <i
+                                <strong>{{ $history->user?->name }}</strong> <i
                                     style="font-size: 14px">{{ $history->created_at->format('d.m.Y H:i') }}</i>
                             </span>
                         </div>
