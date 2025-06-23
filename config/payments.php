dcfd659059815e02ce7525d9481e1b2ede41f509
Khusrav-Muhammadi <khusravmuhammadi.73@gmail.com>
2025-06-23 18:11:10 +0500

    fg

--- config/payments.php
+++ config/payments.php
@@ -3,7 +3,8 @@
 
 return [
     'alif' => [
-        'token' => 'vaCx7qkNNahFb9LhN+ZF2EqhT/+9c8uK5LrGXvGoG/yf',
+        'token' => 'Yw/KJo4zFR8Lizpn4a37i6a80zRKesnTYbf4R6Ypz0Bj',
+        'token_test' => 'vaCx7qkNNahFb9LhN+ZF2EqhT/+9c8uK5LrGXvGoG/yf',
         'url_test' => 'https://api-dev.alifpay.uz/v2/invoice',
         'url' => 'https://api.alifpay.uz/v2/invoice',
         'payment_page' => 'https://checkout-dev.alifpay.uz?invoice='
