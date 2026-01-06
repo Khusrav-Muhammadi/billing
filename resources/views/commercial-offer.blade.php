673f982bab71138622939dd2c51504a48d813e50
FTursunboy <tfaiziev04@gmail.com>
2026-01-06 21:26:08 +0500

    changes

--- resources/views/commercial-offer.blade.php
+++ resources/views/commercial-offer.blade.php
@@ -619,7 +619,7 @@
 <div class="page-content">
     <div class="page-header">
         <img class="page-logo" src="https://billing-back.shamcrm.com/img/logoWithText.png" alt="SHAM CRM">
-        <div class="page-link">*подробнее: <a href="https://shamcrm.com" target="_blank">shamcrm.com</a></div>
+        <div class="page-link">*подробнее: <a href="https://shamcrm.com/price" target="_blank">shamcrm.com</a></div>
     </div>
 
     <!-- TARIFF SECTION -->
@@ -770,7 +770,7 @@
                         <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
                     </svg>
                 </div>
-                <div class="contact-value">{{ $contacts['phone'] ?? '+998 78 555 7416' }}</div>
+                <div class="contact-value">+998 78 555 7416</div>
             </div>
 
             <div class="contact-item">
