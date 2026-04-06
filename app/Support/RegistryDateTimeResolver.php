ec278fd39831ff3841627120078cad8662187068
FTursunboy <tfaiziev04@gmail.com>
2026-04-06 15:22:17 +0500

    chages

--- app/Support/RegistryDateTimeResolver.php
+++ app/Support/RegistryDateTimeResolver.php
@@ -10,8 +10,8 @@ class RegistryDateTimeResolver
 {
     public static function resolve(CommercialOffer $offer, CommercialOfferStatus $status): Carbon
     {
-        $baseDate = $offer->status_date
-            ?: $status->status_date
+        $baseDate = $status->status_date
+            ?: $offer->status_date
             ?: now();
 
         $timeSource = $status->created_at ?: now();
