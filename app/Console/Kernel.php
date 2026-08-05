090d867ef46b6335b2e5d13f8d5d920ef5da89e6
root <tfaiziev04@gmail.com>
2026-08-05 10:39:26 +0500

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,10 +22,10 @@ class Kernel extends ConsoleKernel
         $schedule->command('app:create-day-closing')->dailyAt('23:50')->withoutOverlapping();
 
         // AI Billing
-        $schedule->command('app:ai-fetch-and-bill')->everyThirtyMinutes()->withoutOverlapping();
-        $schedule->command('app:ai-end-of-month')->dailyAt('23:59')->withoutOverlapping();
-        $schedule->command('app:ai-start-of-month')->dailyAt('00:01')->withoutOverlapping();
-        $schedule->command('app:ai-check-scheduled')->dailyAt('09:00')->withoutOverlapping();
+      //$schedule->command('app:ai-fetch-and-bill')->everyThirtyMinutes()->withoutOverlapping();
+      //  $schedule->command('app:ai-end-of-month')->dailyAt('23:59')->withoutOverlapping();
+      //  $schedule->command('app:ai-start-of-month')->dailyAt('00:01')->withoutOverlapping();
+      //  $schedule->command('app:ai-check-scheduled')->dailyAt('09:00')->withoutOverlapping();
     }
 
     /**
