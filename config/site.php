04f204d82f8cd0733db85e4256621de8796918c5
root <tfaiziev04@gmail.com>
2026-08-26 15:39:42 +0500

    cajgems

--- config/site.php
+++ config/site.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'api_token' => env('SITE_API_TOKEN', ''),
+    'api_token' => 'site_2dbc42bbe9fff16f951ba218cb5fd5b37c8627cee47545f3',
     'token_header' => 'X-Site-Token',
 ];
